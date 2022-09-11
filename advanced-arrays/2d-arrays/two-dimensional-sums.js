715cbdfea9141256a9fdb0f441c039f2b3943984
Darius Moore <dariusmoore@dariuss-air.myfiosgateway.com>
2022-09-11 18:52:25 -0400

    2d array exercises

---
+++ advanced-arrays/2d-arrays/two-dimensional-sums.js
@@ -0,0 +1,29 @@
+// your code here
+function twoDimensionalSum(arr) {
+    let sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        let row = arr[i];
+        
+        for (let j = 0; j < row.length; j++) {
+            sum = sum + row[j];
+        }
+     }
+     
+     return sum;
+  }
+  
+  
+  
+  let arr1 = [
+      [1, 3],
+      [-4, 7, 10],
+      [2]
+  ];
+  console.log(twoDimensionalSum(arr1)); // 19
+  
+  let arr2 = [
+      [],
+      [3, 1, 2],
+  ];
+  console.log(twoDimensionalSum(arr2)); // 6
