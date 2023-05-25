a77691add8ea6ea4dd680382dc6507d3c0b8df9d
Darius Moore <iamdariusmoore@gmail.com>
2023-05-25 15:25:22 -0400

    basic loops: least common multiple exercise

---
+++ Javascript-Fundamentals/control-flow/basic-loops/least-common-multiple.js
@@ -0,0 +1,17 @@
+
+function leastCommonMultiple(num1, num2) {
+    let maxNum = num1 * num2;
+    let i = 1;
+    
+    while (i <= maxNum) {
+        if ( (i % num1 === 0) && (i % num2 === 0) ) {   
+            return i;
+        }
+        i++;
+    }
+}
+
+
+console.log(leastCommonMultiple(4, 6)); // 12
+console.log(leastCommonMultiple(3, 5)); // 15
+console.log(leastCommonMultiple(2, 10)); // 10
